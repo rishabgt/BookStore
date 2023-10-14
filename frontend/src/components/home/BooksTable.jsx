47abb05416220c336e71b735050f2cc08aec1306
Rishab Prasad <rishabgt@gmail.com>
2023-10-14 23:37:16 +0530

    Format card to add generic card & remove warn

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -22,7 +22,7 @@ const BooksTable = ({ books }) => {
       </thead>
       <tbody>
         {books.map((book, index) => (
-          <tr key={book.id} className="h-8">
+          <tr key={book._id} className="h-8">
             <td className="border border-slate-700 rounded-md text-center">
               {index + 1}
             </td>
