adccecefba8f820958e058e0289baf88f45ec422
Rishab Prasad <rishabgt@gmail.com>
2023-10-31 21:13:58 +0530

    Added Bookmodal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,6 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 import BooksCard from "../components/home/BooksCard";
 import BooksTable from "../components/home/BooksTable";
@@ -13,7 +11,6 @@ const Home = () => {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(false);
   const [showType, setShowType] = useState("table");
-  console.log("RENDER..");
   useEffect(() => {
     let viewType = sessionStorage.getItem("view");
     if (viewType === null) {
